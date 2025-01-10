91ce8ad5770815cdeecb091b0a17578ced2923df
Jelle Hak <jellehak@gmail.com>
2025-01-10 12:09:53 +0100

    don't render common binary files

--- src/lib/File.js
+++ src/lib/File.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import mime from 'mime-types'
 
 export class File {
   path = ''
@@ -9,7 +10,41 @@ export class File {
     Object.assign(this, obj)
   }
 
+  isBinary() {
+    // Use mime-types for more accurate detection
+    const mimeType = mime.lookup(this.path)
+    
+    // List of mime types considered binary
+    const binaryMimeTypes = [
+      'image/', 
+      'audio/', 
+      'video/', 
+      'application/pdf',
+      'application/zip',
+      'application/gzip',
+      'application/x-tar',
+      'application/octet-stream',
+      'application/x-executable',
+      'application/x-sharedlib',
+      'application/x-object'
+    ]
+
+    // Check mime type
+    if (mimeType && binaryMimeTypes.some(type => mimeType.startsWith(type))) {
+      return true
+    }
+
+    return false
+  }
+
   render () {
+    // Check if file is binary
+    if (this.isBinary()) {
+      return `##### \`${this.path}\`
+[Binary file]
+`
+    }
+
     const ext = path.extname(this.path).slice(1) || 'txt'
     return `##### \`${this.path}\`
 \`\`\`${ext}
