649b6971428b5da580532737f694137155cc8104
Jelle Hak <jellehak@gmail.com>
2025-01-12 12:24:33 +0100

    refactor: enhance command execution flow by using promises for better error handling

--- src/lib/tools/cli.js
+++ src/lib/tools/cli.js
@@ -4,39 +4,48 @@ import { Tool } from './Tool.js'
 import { ACTION_TAG } from '../prompt.js'
 import chalk from 'chalk'
 
+function execPromise(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+                return
+            }
+            resolve({ stdout, stderr })
+        })
+    })
+}
+
 export const cliTool = new Tool({
     name: 'cli',
     version: '0.0.0beta',
     examples: `
 <${ACTION_TAG} tool="cli" command="npm init"></${ACTION_TAG}>`,
     onFinish(res, tools) {
-        console.log(res)
         tools.queue.push(async (ctx) => {
             // ask the user first using inquirer
-            await inquirer.prompt([
+            const answers = await inquirer.prompt([
                 {
                     type: 'confirm',
                     name: 'confirm',
                     message: `Are you sure you want to run the command: \`${chalk.blue(res.command)}\`?`,
                     default: false
                 }
-            ]).then(answers => {
-                if (answers.confirm) {
-                    exec(res.command, (error, stdout, stderr) => {
-                        if (error) {
-                            console.error(`Error executing command: ${error.message}`)
-                            return
-                        }
-                        if (stderr) {
-                            console.error(`Command stderr: ${stderr}`)
-                            return
-                        }
-                        console.log(`Command stdout: ${stdout}`)
-                    })
-                } else {
-                    console.log('Command execution cancelled by user.')
+            ])
+            if (answers.confirm) {
+                try {
+                    const { stdout, stderr } = await execPromise(res.command)
+                    if (stderr) {
+                        console.error(`Command stderr:\n${stderr}`)
+                        return
+                    }
+                    console.log(`Command stdout:\n${stdout}`)
+                } catch (error) {
+                    console.error(`Error executing command: ${error.message}`)
                 }
-            })
+            } else {
+                console.log('Command execution cancelled by user.')
+            }
         })
     }
 })
