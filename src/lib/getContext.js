603c579f7da5268cf1b9745a7773f410512f93a3
Jelle Hak <jellehak@gmail.com>
2025-01-12 10:44:47 +0100

    refactor: simplify getContext function by removing size limit checks and binary file filtering

--- src/lib/getContext.js
+++ src/lib/getContext.js
@@ -1,29 +1,13 @@
 import { gatherContextInfo } from './fs.js'
-import { LimitChecker } from './LimitChecker.js'
 
 /**
- * Gets the context files while applying size limits and skipping binary files
+ * Gets the context files
  * @param {object} config Configuration object
  * @returns {Promise<Array>} Array of context files
  */
 export async function getContext(config) {
-  const limitChecker = new LimitChecker(config)
   const contextInfo = await gatherContextInfo(config.include, config)
 
-  // Filter and validate files
-  const validFiles = contextInfo.filter(file => {
-    try {
-      // Skip binary files
-      // if (isBinaryPath(file.path)) return false
-      
-      // Check size limits
-      limitChecker.checkFile(file.path, file.size)
-      return true
-    } catch (error) {
-      return false
-    }
-  })
-
-  return validFiles
+  return contextInfo
 }
 
